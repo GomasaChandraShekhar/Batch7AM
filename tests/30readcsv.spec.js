26410df030bc13648351d8fe352f465ff178dd66
GomasaChandraShekhar <gomasachandrashekhar@gmail.com>
2026-05-29 07:21:26 +0530

    Udated readcsv file

--- tests/30readcsv.spec.js
+++ tests/30readcsv.spec.js
@@ -3,41 +3,41 @@ import fs from 'fs';
 import { parse } from 'csv-parse/sync';
 
 const csvFilepath = 'testdata/data.csv';
-const content = fs.readFileSync(csvFilepath, 'utf-8');
-const loginData = parse(content, { columns: true, skip_empty_lines: true });
+const content = fs.readFileSync( csvFilepath, 'utf-8' );
+const loginData = parse( content, { columns: true, skip_empty_lines: true } );
 
-test.describe("Verify Login", () => {
+test.describe( "Verify Login", () => {
 
-    for (const { id, email, password, validity } of loginData) {
+    for ( const { id, email, password, validity } of loginData ) {
 
-        test(`Login Test for ${id}`, async ({ page }) => {
-            await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
-            await page.waitForTimeout(2000);
+        test( `Login Test for ${id}`, async ( { page } ) => {
+            await page.goto( 'https://rahulshettyacademy.com/client/#/auth/login' );
+            await page.waitForTimeout( 2000 );
             // await page.pause();
 
-            await page.locator('#userEmail').fill(email);
-            await page.locator('#userPassword').fill(password);
-            await page.locator('#login').click();
+            await page.locator( '#userEmail' ).fill( email );
+            await page.locator( '#userPassword' ).fill( password );
+            await page.locator( '#login' ).click();
 
-            if (validity == 'valid') {
-                await page.waitForTimeout(2000);
-                await expect.soft(page.getByRole('button', { name: 'HOME' })).toBeVisible();
-                await expect.soft(page.getByText('Automation Practice')).toBeVisible();
-                const signOutButton = page.getByRole('button', { name: 'Sign Out' });
-                await expect.soft(signOutButton).toBeVisible();
+            if ( validity == 'valid' ) {
+                await page.waitForTimeout( 2000 );
+                await expect.soft( page.getByRole( 'button', { name: 'HOME' } ) ).toBeVisible();
+                await expect.soft( page.getByText( 'Automation Practice' ) ).toBeVisible();
+                const signOutButton = page.getByRole( 'button', { name: 'Sign Out' } );
+                await expect.soft( signOutButton ).toBeVisible();
                 await signOutButton.click();
             }
-            else if (validity == 'invalid') {
-                const errorMessage = page.locator('div#toast-container');
-                await expect.soft(errorMessage).toBeVisible();
-                console.log(await errorMessage.innerText());
+            else if ( validity == 'invalid' ) {
+                const errorMessage = page.locator( 'div#toast-container' );
+                await expect.soft( errorMessage ).toBeVisible();
+                console.log( await errorMessage.innerText() );
             }
-            await page.waitForTimeout(2000);
+            await page.waitForTimeout( 2000 );
             await page.close();
 
-        });
+        } );
     }
 
-});
+} );
 
 
